0ce7087049962a64f22cdb41a7eed675cf5e0610
HARSH MITTAL <harshmittalmon@gmail.com>
2025-06-21 21:03:52 +0530

    added get request for default page

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ app.use(cors({
   origin: "http://localhost:3000", // frontend
   credentials: true // this line is required for cookies/localStorage access
 }));
-
+app.get("/", (req, res) => {
+  res.send({
+    active: true,
+    error: false,
+  })
+})
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -17,14 +22,14 @@ app.use((req, res, next) => {
 
 
 
-mongoDB();  
+mongoDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use(express.json());
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
-app.use("/api",require("./Routes/OrderData") );
+app.use("/api", require("./Routes/OrderData"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
