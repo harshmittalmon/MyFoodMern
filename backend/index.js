efcbb52ee0935d86db29f44799399a3fb8ebb194
HARSH MITTAL <harshmittalmon@gmail.com>
2025-06-21 21:14:14 +0530

    vercel deployment necessitites

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,12 @@ app.use(express.json());
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
+
+
+module.exports.handler = serverless(app);
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
