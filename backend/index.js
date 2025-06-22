c5fbd2fcb4924a7b4c09751fbe0603fdd15a39cb
HARSH MITTAL <harshmittalmon@gmail.com>
2025-06-23 00:37:07 +0530

    commented out middleware which was blocking cors function

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,11 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 
